3356ed504e94364f985e6b4e124a32f6eff61258
EddBeltran <edd.bng@gmail.com>
2021-08-30 13:10:34 -0500

    Deploy in hostinger.

--- sendEmail.php
+++ sendEmail.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])){
     $message = $_POST['message'];
     
     
-    $mailTo  = "eduardo.beltran@comunidad.unam.mx";
+    $mailTo  = "hello@eduardobeltran.net";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
