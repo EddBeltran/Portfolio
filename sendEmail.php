73ea54130092ee900a9d6ce686c4573c55e7ea37
EddBeltran <edd.bng@gmail.com>
2021-08-30 11:56:27 -0500

    Delete PHPMailer.

--- sendEmail.php
+++ sendEmail.php
@@ -1,41 +1,17 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
 
-if(isset($_POST['name']) && isset($_POST['email'])){
+if (isset($_POST['submit'])){
     $name = $_POST['name'];
-    $email = $_POST['email'];
-    $body = $_POST['body'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+    
+    
+    $mailTo  = "eduardo.beltran@comunidad.unam.mx";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
 
-    require_once "PHPMailer/PHPMailer.php";
-    require_once "PHPMailer/SMTP.php";
-    require_once "PHPMailer/Exception.php";
-
-    $mail = new PHPMailer();
-
-    //smtp settings
-    $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
-    $mail->SMTPAuth = true;
-    $mail->Username = "eddbng.portfolio@gmail.com";
-    $mail->Password = 'navigo97*';
-    $mail->Port = 465;
-    $mail->SMTPSecure = "ssl";
-
-    //email settings
-    $mail->isHTML(true);
-    $mail->setFrom($email, $name);
-    $mail->addAdress("eddbng.portfolio@gmail.com");
-    $mail->Subject = ("Somethig from upsidedown");
-    $mail->Body = $body;
-
-    if($mail->send()){
-        $status = "success";
-        $response = "Email is sent!";
-    }
-    else {
-        $status = "failed";
-        $response = "Something is wrong: <br>" .$mail->ErrorInfo;
-    }
-    exit(json_encode(array("status" => $status, "response" => $response)));
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
 }
 ?>
