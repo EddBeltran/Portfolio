0c8f6951c0bd29f892cd666decfc29cd52ac296a
EddBeltran <edd.bng@gmail.com>
2021-09-12 19:54:58 -0500

    Contact form added.

--- sendEmail.php
+++ sendEmail.php
@@ -2,16 +2,17 @@
 
 if (isset($_POST['submit'])){
     $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
+    $subject = "Portfolio Email";
+    $mailFrom = $_POST['email'];
     $message = $_POST['message'];
     
-    
     $mailTo  = "hello@eduardobeltran.net";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
+    $headers = "From: " . $mailFrom. "\r\n";  
+    $headers .= 'Content-Type: text/plain; charset=utf-8' . "\r\n";
 
+    $txt = $message . "\n\n From: ". $name;
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html?mailsend");
+    header("Location: index.html?emailsent#form");
+
 }
 ?>
