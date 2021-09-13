2c20597d2ee2e101186b60f72359e294ea3f2640
EddBeltran <edd.bng@gmail.com>
2021-09-12 19:57:19 -0500

    Added javascript functions for contact form.

---
+++ functions.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+    const emailSent = window.location.search;
+    if (emailSent === '?emailsent')
+      showEmailSent();
+    else 
+      document.getElementById("submit-notification").innerHTML = "";
+  };
+  
+  function showEmailSent(){
+    document.getElementById("submit-notification").innerHTML = "Email sent successfully!";
+    setTimeout(hideEmailSent, 5000);
+  }
+  
+  function hideEmailSent() {
+    $("#submit-notification").fadeOut().empty();
+    history.pushState(null, "", "index.html");
+  }
+  
+  const tx = document.getElementsByTagName("textarea");
+  for (let i = 0; i < tx.length; i++) {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    tx[i].addEventListener("input", OnInput, false);
+  }
+  
+  
+  function OnInput() {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+  }
+  
+  $(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        var $this = $(this),
+        $body = $('html, body'),
+        target = $($this.attr('href'));
+        event.preventDefault();
+        $body.animate({ scrollTop: target.offset().top - 50 });
+      } // End if
+    });
+  });
+  
+  
+  const form = document.getElementById('form');
+  form.addEventListener('focus', (event) => {
+    event.target.style.background = 'gainsboro';
+    document.getElementById("set_form_align").click();
+  }, true);
+  
+  form.addEventListener('blur', (event) => {
+    event.target.style.background = '';
+  }, true);
